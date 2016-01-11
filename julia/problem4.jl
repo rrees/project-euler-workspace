16f2ac31b03b3d66372bf89258e79e4a2443467f
Robert Rees <shudderfix@gmail.com>
2016-01-11 23:50:32 +0000

    Creates a working solution to problem 4

--- julia/problem4.jl
+++ julia/problem4.jl
@@ -1,6 +1,22 @@
 
-for i in 100:999
-	for j in 100:999
-		println(i, " ", j)
-	end
+function palindrome(i)
+	return string(i) == reverse(string(i))
 end
+
+function find_largest_palindrome()
+	largest_palindrome = 0
+	for i in 100:999
+		for j in 100:999
+			product = i * j
+			if (product > largest_palindrome) && palindrome(product)
+				largest_palindrome = product
+			end
+		end
+	end
+
+	return largest_palindrome
+end
+
+total = find_largest_palindrome()
+
+println(total)
